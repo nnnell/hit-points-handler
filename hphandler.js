05d44e884afa1df960f21ea7d6d03e43d820ece7
internell <jethine@gmail.com>
2021-03-05 12:34:37 -0600

    lookin good

--- hphandler.js
+++ hphandler.js
@@ -1,89 +1,136 @@
-// Let's call the head and torso tier 1 parts, and the limbs tier 2.
+// I'm probably making this overly complicated, but let's call the head and torso tier 1 parts, and the limbs tier 2.
 //
 // Rules:
 // -> HP change in any part will affect the total only if the HP of that part is above 0.
 // -> If any tier 2 part is less than 0, add its negative value to the torso.
 // -> If either tier 1 part is less than 0, add its negative value to the other tier 1 part.
+// -> If a limb is dismembered while its HP is above 0, its value is subtracted from the total.
 
 const hitPointsHandler = {
-  hpTotal: document.querySelector('[data-hp="total"]'),
+  hpTotal: document.querySelector('[data-hp-id="total"]'),
+  // hpFields: document.querySelectorAll('[data-hp-init]'),
   hpTier1: document.querySelectorAll('[data-tier="1"]'),
   hpTier2: document.querySelectorAll('[data-tier="2"]'),
-  resetBtn: document.querySelector('[data-hp="reset"]'),
-  
+  resetBtn: document.querySelector('[data-hp-id="reset"]'),
+
   deathConditions: {
     total: x => x <= 0,
     // head: x => x < -30,
     // torso: x => x < -30
   },
 
-  initialHitPoints: {
-    "head": {
-      "value": 20,
-      "severed": false
-    },
-    "torso": {
-      "value": 30,
-      "severed": false
-    },
-    "arm-left": {
-      "value": 10,
-      "severed": false
-    },
-    "arm-right": {
-      "value": 10,
-      "severed": false
-    },
-    "leg-left": {
-      "value": 15,
-      "severed": false
-    },
-    "leg-right": {
-      "value": 15,
-      "severed": false
-    }
+  // initialHitPoints: {
+    // "head": {
+      // "value": parseInt(document.querySelector('[data-hp-id="head"]').getAttribute('data-hp-init')),
+      // "severed": false
+    // },
+    // "torso": {
+      // "value": 30,
+      // "severed": false
+    // },
+    // "arm-left": {
+      // "value": 10,
+      // "severed": false
+    // },
+    // "arm-right": {
+      // "value": 10,
+      // "severed": false
+    // },
+    // "leg-left": {
+      // "value": 15,
+      // "severed": false
+    // },
+    // "leg-right": {
+      // "value": 15,
+      // "severed": false
+    // }
+  // },
+
+  // hitPoints: {},
+
+  strIsTrue(string) {
+    return string.toLowerCase() == 'true'
   },
-  hitPoints: {},
-  
+
+  // setupHitPoints() {
+    // let self = this
+// 
+    // Array.from(self.hpFields).forEach(field => {
+      // let fieldId = field.getAttribute('data-hp-id')
+      // let limbToggle = document.querySelector(`[data-sever=${fieldId}]`) || undefined
+// 
+      // this.hitPoints[fieldId] = {
+        // "value": parseInt(field.getAttribute('data-hp-init')),
+        // "severed": limbToggle ? self.strIsTrue(limbToggle.getAttribute('data-sever-init')) : false
+      // }
+// 
+    // })
+  // },
+
   initialize() {
+    // Create the hitPoints object:
+    this.hitPoints = Object.create({})
+
     // Set up values and render them:
     this.reset() 
-    
+
     // Bind these event handlers to the right scope:
     let handleInputChange = this.handleInputChange.bind(this)
     let reset = this.reset.bind(this)
-    
+
     // Set up event listeners:
     document.addEventListener('change', handleInputChange)
     this.resetBtn.addEventListener('click', reset)
   },
 
   reset() {
-    let that = this
-    
+    let self = this
+
     // Overwrite the current hitPoints object with initial values:
-    that.hitPoints = JSON.parse(JSON.stringify(that.initialHitPoints))
-    // And then render it:
-    this.renderValues(that.hitPoints)
-    // Make sure everything is rendered as reattached, too:
-    Object.keys(that.hitPoints).map((part) => {
-      if (document.querySelector(`[data-sever="${part}"]`))
-        document.querySelector(`[data-sever="${part}"]`).checked = false
-      that.handleDismemberment(that.hitPoints, part)
-    })
+    fetch('initialHitPoints.json')
+      .then(response => response.text())
+      .then(data => {
+        let hp = JSON.parse(data)
+        self.hitPoints = hp
+
+        // And then render it:
+        self.renderValues(self.hitPoints)
+
+        // Set upper bounds:
+        self.setInputMaximums(self.hitPoints)
+
+        // And render default state of limb attachment:
+        Object.keys(self.hitPoints).map((part) => {
+          if (document.querySelector(`[data-sever="${part}"]`)) {
+            let limbToggle = document.querySelector(`[data-sever="${part}"]`)
+            // limbToggle.checked = self.strIsTrue(limbToggle.getAttribute('data-sever-init'))
+            limbToggle.checked = self.hitPoints[part]['severed']
+          }
+          self.handleDismemberment(self.hitPoints, part)
+        })
+      })
+
   },
-  
+
+  setInputMaximums(hitPoints) {
+    for (let part in hitPoints) {
+      if (hitPoints.hasOwnProperty(part)) {
+        document.querySelector(`[data-hp-id=${part}]`).setAttribute('max', hitPoints[part]['value']) 
+      }
+    }
+  },
+
   handleInputChange(event) {
-    if (!event.target.getAttribute('data-hp') && !event.target.getAttribute('data-sever')) return
-    
-    if (event.target.getAttribute('data-hp')) {
-      let changedPart = event.target.getAttribute('data-hp')
+    if (!event.target.getAttribute('data-hp-id') && !event.target.getAttribute('data-sever')) return
+
+    if (event.target.getAttribute('data-hp-id')) {
+      let changedPart = event.target.getAttribute('data-hp-id')
       let tier = event.target.getAttribute('data-tier')
       let partStartingValue = this.hitPoints[changedPart]['value']
-      
+
       // Update the hit points for the body part in question:
       this.hitPoints[changedPart]['value'] = parseInt(event.target.value)
-      
+
       // Hand off the update to calculate what else it affects:
       this.calculateValues(
         this.hitPoints,
@@ -92,22 +139,25 @@ const hitPointsHandler = {
         partStartingValue
       )
     }
-    
+
     if (event.target.getAttribute('data-sever')) {
       let affectedPart = event.target.getAttribute('data-sever')
       this.hitPoints[affectedPart]['severed'] = event.target.checked
       this.handleDismemberment(this.hitPoints, affectedPart)
     }
-    
+
   },
-  
-  handleCascade(hitPoints, cascadeTarget, prevValue, updatedValue) {
+
+  handleCascade(
+    hitPoints,
+    cascadeTarget,
+    prevValue,
+    updatedValue
+  ) {
     let increment = prevValue - updatedValue
     let targetValue = hitPoints[cascadeTarget]['value']
     let isOverMaximum = (x, target) => x > this.initialHitPoints[target]['value']
 
-    console.log(`handleCascade for ${cascadeTarget}: ${prevValue} is changing to ${updatedValue}`)
-    
     // Handle a decrease:
     if (increment > 0 && updatedValue < 0) {
       if (prevValue >= 0) {
@@ -130,39 +180,46 @@ const hitPointsHandler = {
       }
     }
   },
-  
-  calculateValues(hitPoints, changedPart, tier, prevValue) {
+
+  calculateValues(
+    hitPoints,
+    changedPart,
+    tier,
+    prevValue,
+    eventType = 'damage'
+  ) {
     let updatedValue = hitPoints[changedPart]['value']
-    
-    // Determine if the torso is getting impacted by limb damage
-    if (parseInt(tier) === 2) {
+
+    // Determine if the torso is getting impacted by limb damage,
+    // which only applies if said limb is still attached
+    if (parseInt(tier) === 2 && eventType !== 'dismemberment') {
       let torsoPrevValue = hitPoints['torso']['value']
       this.handleCascade(hitPoints, 'torso', prevValue, updatedValue)
-      
+
       // Determine if this will also affect the head
       this.handleCascade(hitPoints, 'head', torsoPrevValue, hitPoints['torso']['value'])
     }
-    
+
     // Determine if the head or torso is affecting the other
     if (parseInt(tier) === 1) {
       let cascadeTarget = changedPart === 'head' ? 'torso' : 'head'
       this.handleCascade(hitPoints, cascadeTarget, prevValue, updatedValue)
     }
-    
+
     this.renderValues(hitPoints)
   },
-  
+
   renderValues(hitPoints) {
     // Render each body part's hit points according to the values in the hitPoints object:
     for (let part in hitPoints) {
       if (hitPoints.hasOwnProperty(part)) {
-        document.querySelector(`[data-hp=${part}]`).value = hitPoints[part]['value']
+        document.querySelector(`[data-hp-id=${part}]`).value = hitPoints[part]['value']
       }
     }
     // And finally update the hit point total:
     this.updateTotal(hitPoints)
   },
-  
+
   updateTotal(hitPoints) {
     let pointsSum = Object.values(hitPoints).reduce(
       (acc, part) => {
@@ -180,49 +237,54 @@ const hitPointsHandler = {
     // And now check to see if you are still alive:
     this.handleAliveness(hitPoints, pointsSum)
   },
-  
-  handleDismemberment(hitPoints, affectedPart) {
+
+  handleDismemberment(
+    hitPoints,
+    affectedPart
+  ) {
     let isSevered = hitPoints[affectedPart]['severed']
     let affectedPartValue = hitPoints[affectedPart]['value']
-    console.log('heard handleDismemberment for', affectedPart, isSevered)
-    console.log('how many hp does ', affectedPart, 'have remaining?', affectedPartValue)
 
     this.calculateValues(
       hitPoints,
       affectedPart,
       2,
-      0
+      0,
+      'dismemberment'
     )
 
     isSevered
-      ? document.querySelector(`[data-hp="${affectedPart}"]`).setAttribute('disabled', true)
-      : document.querySelector(`[data-hp="${affectedPart}"]`).removeAttribute('disabled')
+      ? document.querySelector(`[data-hp-id="${affectedPart}"]`).setAttribute('disabled', true)
+      : document.querySelector(`[data-hp-id="${affectedPart}"]`).removeAttribute('disabled')
   },
-  
-  handleAliveness(hitPoints, total) {
+
+  handleAliveness(
+    hitPoints,
+    total
+  ) {
     if (this.deathConditions['total'](total)) {
+      // You're dead
       document.querySelector('[data-alive]').setAttribute('data-alive', false)
-      document.querySelector('[data-hp="total"]').classList.add('is-dead')
-      // You're dead, get out
+      document.querySelector('[data-hp-id="total"]').classList.add('is-dead')
       return
     }
-    
+
     // Do any of the values in the current hitPoints object match any deathConditions?
     for (let part in this.deathConditions) {
       // Only check the parts in hitPoints that are mentioned in deathConditions:
       if (hitPoints.hasOwnProperty(part)) {
         if (this.deathConditions[part](hitPoints[part])) {
+          // You're dead
           document.querySelector('[data-alive]').setAttribute('data-alive', false)
-          document.querySelector(`[data-hp="${part}"]`).classList.add('is-dead')
-          // You're dead, get out
+          document.querySelector(`[data-hp-id="${part}"]`).classList.add('is-dead')
           return
         }
       }
     }
-    
+
     // Made it this far? Guess you're still alive, good job!
     document.querySelector('[data-alive]').setAttribute('data-alive', true)
-    ;[...document.querySelectorAll('[data-hp]')].map((element) => {
+    Array.from(document.querySelectorAll('[data-hp-id]')).map((element) => {
       element.classList.remove('is-dead')
     })
   }
